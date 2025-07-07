6581431093e7e57a64a67c0d9727514c679fd7a0
user <wjdghdud999@gmail.com>
2025-07-07 17:15:47 +0900

    동작 테스트

---
+++ src/main/java/com/fastcampus/langchain_chatbot/LangChainChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.langchain_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangChainChatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangChainChatBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/langchain_chatbot/LangChainChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.langchain_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangChainChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
